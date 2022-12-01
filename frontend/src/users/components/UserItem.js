fe56c33822c48f209c7c6f45ade37bccc9b9ef9d
David Afdal Kaizar Mutahadi <davidafdal7@gmail.com>
2022-12-01 16:34:01 +0900

    update

--- frontend/src/users/components/UserItem.js
+++ frontend/src/users/components/UserItem.js
@@ -15,7 +15,7 @@ const UserItem = (props) => {
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
-              {props.placesCount} {props.placesCount === 1 ? "place" : "places"}
+              {props.placesCount} {props.placesCount <= 1 ? "place" : "places"}
             </h3>
           </div>
         </Link>
